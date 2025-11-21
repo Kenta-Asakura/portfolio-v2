b24184099fcb40a200440436c685bdeee0b078e2
Kenta Asakura <asakura.kenta963420@gmail.com>
2025-11-21 23:53:25 +0900

    Header code cleaned up

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -5,8 +5,6 @@ import { SOCIAL_LINKS } from "../../data/social";
 function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [activeSection, setActiveSection] = useState('');
-  // const [showSocialLinks, setShowSocialLinks] = useState(false);
-  const [showSocialLinks, setShowSocialLinks] = useState(true); // !TEST
 
   // Smooth scroll handler
   const handleNavClick = (e) => {
@@ -186,9 +184,8 @@ function Header() {
               ))}
             </ul>
 
-
             {/* Social and Chatbot Buttons */}
-            <div className="d-fab items-center">
+            <div className="max-lg:flex-row d-fab items-center">
               {/* Social Toggle Button */}
               <div 
                 tabIndex={0} 
@@ -215,38 +212,38 @@ function Header() {
               {SOCIAL_LINKS.map((social) => (
                 <button 
                   id="btn social-links-section"
-                  className={`lg:${showSocialLinks ? '' : 'hidden'}
+                  className={`
                     flex lg:flex-col items-center gap-2 animate-fade-in
                     d-btn d-btn-lg d-btn-circle
-                    `}
+                  `}
                 >
-                    <a
-                      key={social.name}
-                      href={social.href}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="
-                        flex items-center justify-center
-                        w-10 h-10 rounded-full
-                        bg-gray-500 hover:bg-base-300
-                        hover:text-primary transition-colors
-                        focus:outline-none focus:ring-2 focus:ring-primary
-                        text-sm"
-                      aria-label={`Visit my ${social.name} profile`}
-                    >
-                      <span className="font-bold text-xl">
-                        {social.icon}
-                        <svg
-                          aria-hidden="true"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="currentColor"
-                          viewBox="0 0 24 24"
-                          className="size-6"
-                        >
-                          <path d={social.iconPath} />
-                        </svg>
-                      </span>
-                    </a>
+                  <a
+                    key={social.name}
+                    href={social.href}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="
+                      flex items-center justify-center
+                      w-10 h-10 rounded-full
+                      bg-gray-500 hover:bg-base-300
+                      hover:text-primary transition-colors
+                      focus:outline-none focus:ring-2 focus:ring-primary
+                      text-sm"
+                    aria-label={`Visit my ${social.name} profile`}
+                  >
+                    <span className="font-bold text-xl">
+                      {social.icon}
+                      <svg
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 24 24"
+                        className="size-6"
+                      >
+                        <path d={social.iconPath} />
+                      </svg>
+                    </span>
+                  </a>
                 </button>
               ))}
 
