a1a5409efa100e4fa7c5097e0d2f49bea23f0d2a
Kenta Asakura <asakura.kenta963420@gmail.com>
2025-12-06 15:25:06 +0900

    Section header component changes: - divider/border removed and replaced with DaisyUI Divider component

--- src/components/ui/SectionHeader.jsx
+++ src/components/ui/SectionHeader.jsx
@@ -1,10 +1,13 @@
 function SectionHeader({ headerTitle, headerNumber }) {
   return (
     <div className="text-upper max-w-4xl mx-auto mb-12">
-        <h2 className="text-4xl md:text-4xl font-semibold uppercase tracking-wider text-neutral-content">
-            <span className="text-primary font-primary text-xl mr-2">{headerNumber}</span>{headerTitle}
+        <h2 className="
+          d-divider d-divider-start
+          text-3xl font-semibold uppercase 
+          tracking-wider text-neutral-content
+        ">
+            <span className="text-primary font-primary text-xl">{headerNumber}</span>{headerTitle}
         </h2>
-        <div className="h-px bg-base-300 mt-4 ml-24"></div>
     </div>
   )
 }
