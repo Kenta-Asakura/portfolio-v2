a60584b9cefb2e839f210b2bd213fb21d3f83b02
Kenta Asakura <asakura.kenta963420@gmail.com>
2025-12-07 00:01:22 +0900

    vite config file fix: -removed autoprefixer as automatically handled by Tailwind engine

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
   plugins: [
     react(), 
-    tailwindcss(), 
-    autoprefixer()
+    tailwindcss(),
   ],
 })
