db77d5678ba7fe7a1f0ee0f77a31b26b18a37fea
Kenta Asakura <asakura.kenta963420@gmail.com>
2025-12-07 00:09:22 +0900

    Section components render in main App component fix test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Layout from './components/layout/Layout';
 import Hero from './components/Sections/Hero';
 import About from './components/Sections/About';
-import Projects from './components/sections/Projects';
+import Projects from './components/Sections/Projects';
 import Experience from './components/Sections/Experience';
 import Contact from './components/Sections/Contact';
 
