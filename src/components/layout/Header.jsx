4b4e3fb7c66d11808ff6d585cf3a75888cb06dbc
Kenta Asakura <asakura.kenta963420@gmail.com>
2025-12-14 00:06:13 +0900

    refactor: Header unused State removed

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -3,27 +3,6 @@ import MobileNav from './MobileNav';
 import DesktopSidebar from './DesktopSidebar';
 
 function Header() {
-  // Smooth scroll handler
-  const handleNavClick = (e) => {
-    setIsMenuOpen(false);
-    const href = e.currentTarget.getAttribute('href');
-
-    if (href?.startsWith('#')) {
-      e.preventDefault();
-      const element = document.querySelector(href);
-
-      if (element) {
-        const offset = 20;
-        const elementPosition = element.offsetTop - offset;
-        
-        window.scrollTo({
-          top: elementPosition,
-          behavior: 'smooth'
-        });
-      }x``
-    }
-  };
-
   return (
      <>
       <MobileNav />
